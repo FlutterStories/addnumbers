2ef86e5528e7db5b146a9fd074f4541222855b60
Anna Jones <anna@Annas-MacBook-Air.local>
2021-11-09 00:58:16 +0530

    Added two numbers using 3rd Variable

--- addnumbers.java
+++ addnumbers.java
@@ -2,6 +2,7 @@ class AddNumbers{
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        System.out.println(a+b);
+        int c = a+b;
+        System.out.println(c);
     }
 }
